3c0fdc67cef9e583904e498c63c52157658013fe
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-19 04:10:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def summon_captain_planet(planeteer_calls)
   exclaimed_call = planeteer_calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? 
+  
+  
 end
 
 def find_the_cheese# code an argument here
