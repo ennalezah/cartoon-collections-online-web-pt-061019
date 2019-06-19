e0bb438ddfa08bcdaa21b9d7982e9f5361d1802c
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-19 04:16:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,7 @@ def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  list.include? {|item| item === cheese_types}
+  list.include?(cheese_types)
+  
+  
 end
